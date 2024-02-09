e1370768a13be8f52e247d25f50548772cba1a89
Marius Wilms <marius@factorial.io>
2024-02-09 17:10:54 +0100

    tobey: Debug webhook dispatch, add request URL

--- webhook.go
+++ webhook.go
@@ -26,7 +26,7 @@ type WebhookPayload struct {
 	// we than want to nest all results data under Data as to prevent
 	// collisions with Action and other fields.
 	RequestURL   string `json:"request_url"`
-	ResponseBody []byte `json:"response_body"`
+	ResponseBody []byte `json:"response_body"` // Will be base64 encoded once marshalled.
 }
 
 type WebhookDispatcher struct {
@@ -40,7 +40,6 @@ func NewWebhookDispatcher() *WebhookDispatcher {
 }
 
 func (wd *WebhookDispatcher) Send(webhook *WebhookConfig, res *colly.Response) error {
-	// log.Printf("Sending webhook got for crawl response (%s)... %s", res.Request.URL, webhook.Endpoint)
 
 	payload := &WebhookPayload{
 		Action: "collector.response",
@@ -49,13 +48,16 @@ func (wd *WebhookDispatcher) Send(webhook *WebhookConfig, res *colly.Response) e
 		// initial crawl request, verbatim.
 		Data: webhook,
 
-		// TODO: Pass RequestURL.
-		ResponseBody: res.Body,
+		RequestURL:   res.Request.URL.String(),
+		ResponseBody: res.Body[:100],
 	}
 	body, err := json.Marshal(payload)
 	if err != nil {
 		return err
 	}
+
+	log.Printf("Sending webhook got for crawl response (%s), body length (%d)... %s", res.Request.URL, len(res.Body), webhook.Endpoint)
+
 	req, err := http.NewRequest("POST", webhook.Endpoint, bytes.NewBuffer(body))
 	if err != nil {
 		return err
@@ -72,7 +74,7 @@ func (wd *WebhookDispatcher) Send(webhook *WebhookConfig, res *colly.Response) e
 		if err != nil {
 			log.Printf("Sending webhook ultimately failed: %s", err)
 		} else {
-			log.Printf("Webhook succesfully sent: %s", webhook.Endpoint)
+			// log.Printf("Webhook succesfully sent: %s", webhook.Endpoint)
 		}
 		wd.wg.Done()
 	}()
