763a85ea1d0af7641f60862db9de219f097669cf
Marius Wilms <marius@factorial.io>
2024-02-12 15:45:55 +0100

    tobey: Refactor HTTP handlers

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/redis/go-redis/v9"
@@ -53,60 +49,7 @@ func main() {
 	workers := CreateVisitWorkersPool(ctx, NumVisitWorkers)
 
 	router := mux.NewRouter()
-
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
-		r.Body.Close()
-
-		fmt.Fprint(w, "Hello from Tobey.")
-	}).Methods("GET")
-
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		body, _ := ioutil.ReadAll(r.Body)
-		r.Body.Close()
-
-		w.Header().Set("Content-Type", "application/json")
-		reqID := uuid.New().ID()
-		log.Printf("Handling incoming crawl request (%d)", reqID)
-
-		var req APIRequest
-		err := json.Unmarshal(body, &req)
-		if err != nil {
-			log.Printf("Failed to parse incoming JSON: %s", err)
-
-			result := &APIError{
-				Message: fmt.Sprintf("%s", err),
-			}
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(result)
-			return
-		}
-
-		cconf, err := DeriveCollectorConfigFromAPIRequest(&req)
-		whconf := req.WebhookConfig
-
-		if err != nil {
-			log.Printf("Failed to derivce collector config: %s", err)
-
-			result := &APIError{
-				Message: fmt.Sprintf("%s", err),
-			}
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(result)
-			return
-		}
-
-		log.Printf("Processing request (%x) for (%s)", reqID, req.URL)
-
-		workQueue.PublishURL(reqID, fmt.Sprintf("%s/sitemap.xml", cconf.Root), cconf, whconf)
-		workQueue.PublishURL(reqID, req.URL, cconf, whconf)
-
-		result := &APIResponse{
-			CrawlRequestID: reqID,
-		}
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(result)
-	}).Methods("POST")
+	addRoutes(router)
 
 	log.Printf("Starting HTTP server, listening on %s...", ":8080")
 
