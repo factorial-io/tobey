a2e1f2cbf0827c060a938d0b0a2ae2c7da1825f6
Marius Wilms <marius@factorial.io>
2024-10-22 16:21:03 +0200

    Add license headers

--- jobpromoter.go
+++ jobpromoter.go
@@ -1,3 +1,8 @@
+// Copyright 2024 Factorial GmbH. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 // The job promoter looks at host queues in a round-robin fashion and promotes
--- ratelimitoptimizer.go
+++ ratelimitoptimizer.go
@@ -1,3 +1,8 @@
+// Copyright 2024 Factorial GmbH. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 // The rate limit optimizer takes samples and optimizer the rate limit, keeping
