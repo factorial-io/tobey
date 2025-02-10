c6f627a60ee2d2ec20a8c0c1bd77b78046cb0be3
Marius Wilms <marius@factorial.io>
2025-02-10 16:07:54 +0100

    Prepare otel for refactor

--- otel.go
+++ otel.go
@@ -8,7 +8,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/mariuswilms/tears"
@@ -130,4 +132,59 @@ func newMeterProvider(ctx context.Context) (*metric.MeterProvider, error) {
 			metric.WithInterval(3*time.Second))),
 	)
 	return meterProvider, nil
+}
+
+// GetEnvString gets the environment variable for a key and if that env-var hasn't been set it returns the default value
+func GetEnvString(key string, defaultVal string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		value = defaultVal
+	}
+
+	slog.Debug("Set Environment ", "key", key, "value", value)
+
+	return value
+}
+
+// GetEnvBool gets the environment variable for a key and if that env-var hasn't been set it returns the default value
+func GetEnvBool(key string, defaultVal bool) bool {
+	envvalue := os.Getenv(key)
+	value, err := strconv.ParseBool(envvalue)
+	if len(envvalue) == 0 || err != nil {
+		value := defaultVal
+		return value
+	}
+
+	slog.Debug("Set Environment ", "key", key, "value", value)
+
+	return value
+}
+
+// GetEnvInt gets the environment variable for a key and if that env-var hasn't been set it returns the default value. This function is equivalent to ParseInt(s, 10, 0) to convert env-vars to type int
+func GetEnvInt(key string, defaultVal int) int {
+	envvalue := os.Getenv(key)
+	value, err := strconv.Atoi(envvalue)
+
+	if len(envvalue) == 0 || err != nil {
+		value := defaultVal
+		return value
+	}
+
+	slog.Debug("Set Environment ", "key", key, "value", value)
+
+	return value
+}
+
+// GetEnvFloat gets the environment variable for a key and if that env-var hasn't been set it returns the default value. This function uses bitSize of 64 to convert string to float64.
+func GetEnvFloat(key string, defaultVal float64) float64 {
+	envvalue := os.Getenv(key)
+	value, err := strconv.ParseFloat(envvalue, 64)
+	if len(envvalue) == 0 || err != nil {
+		value := defaultVal
+		return value
+	}
+
+	slog.Debug("Set Environment ", "key", key, "value", value)
+
+	return value
 }
